06fe9ffa3ed8c0e9180b87c19b8e156fe8e19ae6
frontendoniyorbek <tursunovdoniyorbek077@gmail.com>
2022-05-05 23:36:40 -0700

    Add seltTimeout

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 const elModal =document.querySelector('.modal');
 const elModalClose = elModal.querySelector('.js-modal-close');
 
-elModalClose.addEventListener('click',function () {
-  elModal.classList.remove('modal-open');
-});
+if (elModalClose) {
+  elModalClose.addEventListener('click',function () {
+    elModal.classList.remove('modal-open');
+  });
+}
+
+setTimeout (function () {
+  elModal.classList.add('modal-open');
+}, 30000);
